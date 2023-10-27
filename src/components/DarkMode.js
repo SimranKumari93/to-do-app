cfdb08c9d8bc68cf473989107eb074a37072fc6c
simran <simrankumaribodhgaya93@gmail.com>
2023-10-27 23:49:37 +0530

    toggle mode

---
+++ src/components/DarkMode.js
@@ -0,0 +1,22 @@
+import React , { useEffect, useState } from 'react'
+
+function DarkMode() {
+    const [theme , setTheme] = useState("light-theme")
+    const toggleTheme = () => {
+       if(theme === 'dark-theme')
+      { setTheme('light-theme')}
+      else{
+        setTheme('dark-theme')
+      }
+     }
+     useEffect(() => {
+        document.body.className = theme;
+     }, [])
+  return (
+    <>
+         <button className='btn' onClick={toggleTheme}>Toggle Mode On</button>
+    </>
+  )
+}
+
+export default DarkMode

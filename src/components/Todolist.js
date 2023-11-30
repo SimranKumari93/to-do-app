4f8c495c09981af8495b47e7b36d4d51f1c0cb38
XD-Admin07 <chintuks.cks@gmail.com>
2023-11-30 12:33:37 +0530

    edit button fixed

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,94 +1,118 @@
 
 import React, { useState } from 'react';
-
+import Task from './Task';
 function Todolist() {
 
-    const [activity , setActivity ] = useState("")
-    const [ listData, setlistData ] = useState([])
-    const [ task, setTask ] = useState("")
-    const [ taskList , settaskList ] = useState([])
-    const [ edit, setEditid ] = useState(0)
-function addActivity(){
+  const [activity, setActivity] = useState('')
+  const [listData, setlistData] = useState([])
+
+  // const [ edit, setEdit ] = useState([])
+  const [editId, setEditId] = useState(0);
+  function addActivity() {
     // setlistData(...[listData, activity])
     // console.log(listData)
-    setlistData((listData)=>{
-      if(!activity.length)
-      {
-        setActivity([]);
-      }
-      const updatedList = [...listData, activity]
-      console.log(updatedList)
-      setActivity('')
-      return updatedList
+
+    if (activity !== "") {
+
+      setlistData([...listData, { activity, id: `${Date.now()}` }]);
+      setActivity('');
+      /*setlistData((listData)=>{
+        const updatedList = [...listData, activity]
+        console.log(updatedList)
+        setActivity('')
+        return updatedList
+      })*/
+    }
+
+    if (editId) {
+      const editTask = listData.find((i) => i.id === editId);
+      const updatedTasks = listData.map((t) =>
+        t.id === editTask.id ? t = { id: t.id, activity } : { id: t.id, activity: t.activity });
+      setlistData(updatedTasks);
+      setEditId(0);
+      return;
+    }
+
+  }
+
+  const removeActivity = (i) => {
+
+  
+    const updatedListData = listData.filter((elem, id) => {
+      return i !== id;
     })
-}    
-// function handleSubmit(){
-//   if(task !== ""){
-//     settaskList([...taskList, { task, id : `${task}-${Date.now()}`}])
-//     setTask("")
-//   }
-// }
-function removeActivity(i){
-     const  updatedListData = listData.filter((elem, id)=>{
-      return i!== id; 
-     })
-     setlistData(updatedListData)
-}
-if(edit){
-  const editTask = taskList.find((i) => i.id === edit)
-  const updatedTask = taskList.map((t) => 
-  t.id === editTask.id ? t={id : t.id, task} : {id: t.id, task : t.task} )
-  settaskList(updatedTask)
-  setEditid(0)
-  return ;
-}
-function removeAll(){
-  setlistData([])
-}
-function handleEdit(id){
-  const editTask = taskList.find((i) => i.id === id)
-  settaskList(editTask.task)
-  setEditid(id)
-}
+    setlistData(updatedListData);
+
+
+  }
+  /*function addEdit(){
+           const updatedEdit = ((edit)=>{
+            console.log(setEdit)
+           })
+  }*/
+  const Handledit = (id) => {
+    const editTask = listData.find((i) => i.id === id);
+    setActivity(editTask.activity);
+    setEditId(id);
+
+  }
+  function removeAll() {
+    setlistData([])
+  }
+
   return (
     <>
-    <div className='container'>
+      <div className='container'>
         <div className='header'>TO DO LIST</div>
         <div>
-    <input type="text" placeholder="Add-activity" value={activity} onChange={(e)=> setActivity(e.target.value)}/>
-   
-    <button onClick={addActivity}>Add</button>
-    <button onClick={() => handleEdit(task.id)} className='edit-btn'>Edit</button>
-    <input type="date" className='input input-bordered input-secondary w-full max w-xs-schedule-date' />
-    <button className='plus-button'>+</button>
-    </div>
-    
-    {listData !=[] && listData.map((data, i)=> {
+          <input type="text" placeholder="Add-activity" value={activity} onChange={(e) => setActivity(e.target.value)} />
+
+
+          <button onClick={addActivity}>Add</button>
+          {/*<button onClick={addEdit}>Edit</button>*/}
+
+
+          <input type="date" className='input input-bordered input-secondary w-full max w-xs-schedule-date' />
+          <button className='plus-button'>+</button>
+        </div>
+
+        {/*{listData !=[] && listData.map((data, i)=> {
       return (
         <>
        <p key={i}>
           <div className='listData'>{data}</div>
+
+          <button onClick={()=>removeActivity(i)}>Remove(-)</button>
+          <button onClick={()=>Handledit(i)}>Edit</button>
+
           <button onClick={()=>removeActivity(i)}>Remove</button>
+
           {listData.length>= 1 &&  <button onClick={removeAll}>Remove All</button>}
-        </p>
+      </p>*
+       
         </>
       )
-    })}
-   
-    <table className='table w-full'>
-      <thead>
-    <tr>
-      <th>Task</th>
-      <th>DueDate</th>
-      <th>Status</th>
-      <th>Actions</th>
-    </tr>
-    
-    </thead>
-    </table>
-   
-    </div>
-    
+    })}*/}
+
+        {listData.map((activity) => {
+          return <Task activity={activity.activity} removeActivity={removeActivity} removeAll={removeAll}
+            id={activity.id} Handledit={Handledit} />
+        })}
+
+        <table className='table w-full'>
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>DueDate</th>
+              <th>Status</th>
+              <th>Actions</th>
+            </tr>
+
+          </thead>
+        </table>
+
+      </div>
+
     </>
   )
 }
